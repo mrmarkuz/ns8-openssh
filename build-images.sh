973b820ed75e1ec40972b4e6d0ed96d124149732
mrmarkuz <mrmarkuz666@gmail.com>
2026-03-06 14:44:11 +0100

    Add fwadm and image

--- build-images.sh
+++ build-images.sh
@@ -36,10 +36,10 @@ buildah add "${container}" imageroot /imageroot
 buildah add "${container}" ui/dist /ui
 # Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
 buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=traefik@node:routeadm" \
+    --label="org.nethserver.authorizations=traefik@node:routeadm node:fwadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/jmalloc/echo-server:latest" \
+    --label="org.nethserver.images=docker.io/linuxserver/openssh-server:10.2_p1-r0-ls218" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
